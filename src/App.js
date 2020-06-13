2f33bae37e80dd1194ae3ec52b54efe1757647dd
juanmartinez <juanm.jm49@gmail.com>
2020-06-13 15:25:58 -0700

    step6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const Filter = ({filter, onChange}) => {
 
@@ -37,17 +38,22 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
   const [personsToDisplay, setPersonsToDisplay] = useState([...persons]);
+
+  const hook = () => {
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          setPersons(response.data);
+          setPersonsToDisplay(response.data);
+        });
+  };
+
+  useEffect(hook, []);
 
   const addPerson = (event) => {
     event.preventDefault();
